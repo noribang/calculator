64b005b2120d11400094db1eed16276f2a1130f2
noribang <hoonk9387@gmail.com>
2017-05-23 18:00:13 -0500

    add functions to scripts.js to subtract, multiply and divide from user input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 var add = function(number1, number2) {/* Function to add two numbers together.*/
-	return number1 + number2;
+	return "Add = " + (number1 + number2);
 }
 
+var subtract = function(number1, number2) {/* Function to subtract two numbers and return difference.*/
+	return "Subtract = " + (number1 - number2);
+}
+
+var multiply = function(number1, number2) {
+	return "Multiply = " + (number1 * number2);
+}
+
+var divide = function(number1, number2) {
+	return "Divide = " + (number1/number2);
+}
 //#1 Prompts for user input and assign string input to var
 // var number1 = prompt("Enter a number:");// Opens dialog box and prompts for user input 
 // 										   // Assign user input to var number1
@@ -9,10 +20,22 @@ var add = function(number1, number2) {/* Function to add two numbers together.*/
 // 										         //Assign user input to var number2
 
 //#2 Prompts for user input and assigns converted input to var
-var number1 = parseInt(prompt('Enter a number:'));// Converts user input to integer 
-var number2 = parseInt(prompt('Enter another number:'));// Converts user input to integer
+var num1 = parseInt(prompt('Enter a number:'));// Converts user input to integer 
+var num2 = parseInt(prompt('Enter another number:'));// Converts user input to integer
 
-// #1
-// alert(number1); // Opens alert window with var number1 passed in as argument 
-// #2
-alert(add(number1, number2));// opens alert window with var number1 and number2 passed in as arguments
+// #1 Opens alert window with var number1 passed in as argument
+// alert(number1);   
+// #2 Opens alert window that will display return of add().
+alert(add(num1, num2));
+
+// #3 Opens alert window that will display return value of subtract().
+// User input assigned to num1 and num2 passed to subtract() as arguments.
+alert(subtract(num1, num2));
+
+// #4 Opens alert window that will display return value of multiply().
+alert(multiply(num1, num2));
+
+// #5 Opens alert window that will display return value of divide(). 
+alert(divide(num1, num2));
+
+
